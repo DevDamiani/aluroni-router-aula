9a4435b86ec286ef6fb543d890ceb7995ec68ae7
DevDamiani <douggdamiani@gmail.com>
2023-01-03 17:50:40 -0300

    6 - Curso finalizado

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,11 +1,19 @@
 import styles from './Item.module.scss';
 import { Prato } from 'types/prato';
 import TagsPrato from 'components/Tags';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
-    const { title, description, photo } = props;
+
+    const { id, title, description, photo } = props;
+
+    const navidate = useNavigate();
+
     return (
-        <div className={styles.item}>
+        <div 
+            className={styles.item}
+            onClick={ () => navidate(`/prato/${id}`)  }
+        >
             <div className={styles.item__imagem}>
                 <img src={photo} alt={title} />
             </div>
--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,7 +2,10 @@ import styles from './prato.module.scss';
 import stylesTema from 'styles/tema.module.scss';
 import cardapio from 'data/cardapio.json';
 import classNames from 'classnames';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import NotFound from 'pages/NotFound';
+import TagsPrato from 'components/Tags';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
 
@@ -11,47 +14,34 @@ export default function Prato() {
     const prato = cardapio.find( item => item.id === Number(id));
 
     if(!prato){
-        return; 
+        return <NotFound />; 
     }
 
     return (
-        <>
-            <button 
-                className={styles.voltar}
-                onClick={_ => navigate(-1)}
-            >
-                {'< Voltar'}
-            </button>
-            <section className={styles.container}>
-                <h1 className={styles.titulo}>
-                    {prato.title}
-                </h1>
-                <div className={styles.imagem}>
-                    <img src={prato.photo} alt={prato.title} />
-                </div>
-                <div className={styles.conteudo}>
-                    <p className={styles.conteudo__descricao}>
-                        {prato.description}
-                    </p>
-                    <div className={styles.tags}>
-                        <div className={classNames({
-                            [styles.tags__tipo]: true,
-                            [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-                        })}>
-                            {prato.category.label}
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={
+                    <>
+                        <button className={styles.voltar} onClick={() => navigate(-1)}>
+                            {'< Voltar'}
+                        </button>
+                        <div className={styles.container}>
+                            <h1 className={styles.titulo}>
+                                {prato.title}
+                            </h1>
+                            <div className={styles.imagem}>
+                                <img src={prato.photo} alt={prato.title} />
+                            </div>
+                            <div className={styles.conteudo}>
+                                <p className={styles.conteudo__descricao}>
+                                    {prato.description}
+                                </p>
+                            </div>
+                            <TagsPrato {...prato} />
                         </div>
-                        <div className={styles.tags__porcao}>
-                            {prato.size}g
-                        </div>
-                        <div className={styles.tags__qtdpessoas}>
-                            Serve {prato.serving} pessoa {prato.serving == 1 ? '' : 's'}
-                        </div>
-                        <div className={styles.tags__valor}>
-                            R$ {prato.price.toFixed(2)}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
+                    </>
+                } />
+            </Route>
+        </Routes>
     );
 }
